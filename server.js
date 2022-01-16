a1e8191ce2c9f65db4d227deedf28965f342eeff
George Mousa <george.mousa02@gmail.com>
2022-01-15 18:07:48 -0600

    Added patch and delete

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express")
 const app = express()
 const db = require('./database.js')
 
+const md5 = require('md5')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 //Server Port
 const HTTP_PORT = 8000
@@ -50,6 +56,77 @@ app.get("/api/user/:id", (req, res, next) => {
         })
       });
 });
+
+app.post('/api/user/', (req, res, next) => {
+    var errors = []
+    if(!req.body.password){
+        errors.push("No password specified")
+    }
+    if(!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(',')})
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: md5(req.body.password)
+    }
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params = [data.name, data.email, data.password]
+    db.run(sql, params, function(err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id": this.lastID
+        })
+    })
+})
+
+
+app.patch("/api/user/:id", (req, res, next) => {
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : req.body.password ? md5(req.body.password) : null
+    }
+    db.run(
+        `UPDATE user set
+           name = COALESCE(?,name),
+           email = COALESCE(?,email),
+           password = COALESCE(?,password)
+           WHERE id = ?`,
+        [data.name, data.email, data.password, req.params.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
+})
+
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM user WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", changes: this.changes})
+    });
+})
 
 app.use((req,res) => {
     res.status(404)
